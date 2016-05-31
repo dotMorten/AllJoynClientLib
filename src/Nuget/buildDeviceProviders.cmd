0ce16ea49ef20b5e521d0a2c65b9318b967da6bc
mort5161 <mnielsen@esri.com>
2016-05-31 08:30:47 -0700

    Use submodule instead of local source copy of device providers

--- src/Nuget/buildDeviceProviders.cmd
+++ src/Nuget/buildDeviceProviders.cmd
@@ -1,7 +1,7 @@
-XCOPY ..\DeviceProviders\bin\Win32\Release\DeviceProviders\DeviceProviders.winmd DeviceProviders\lib\uap10.0\ /Y
-XCOPY ..\DeviceProviders\bin\Win32\Release\DeviceProviders\DeviceProviders.dll DeviceProviders\runtimes\win10-x86\native\ /Y
-XCOPY ..\DeviceProviders\bin\x64\Release\DeviceProviders\DeviceProviders.dll DeviceProviders\runtimes\win10-x64\native\ /Y
-XCOPY ..\DeviceProviders\bin\ARM\Release\DeviceProviders\DeviceProviders.dll DeviceProviders\runtimes\win10-arm\native\ /Y
+XCOPY ..\Release\DeviceProviders\DeviceProviders.winmd DeviceProviders\lib\uap10.0\ /Y
+XCOPY ..\Release\DeviceProviders\DeviceProviders.dll DeviceProviders\runtimes\win10-x86\native\ /Y
+XCOPY ..\x64\Release\DeviceProviders\DeviceProviders.dll DeviceProviders\runtimes\win10-x64\native\ /Y
+XCOPY ..\ARM\Release\DeviceProviders\DeviceProviders.dll DeviceProviders\runtimes\win10-arm\native\ /Y
 XCOPY dotMorten.AllJoyn.DeviceProviders.nuspec DeviceProviders\ /Y
 nuget pack DeviceProviders\dotMorten.AllJoyn.DeviceProviders.nuspec
 RMDIR DeviceProviders /S /Q
