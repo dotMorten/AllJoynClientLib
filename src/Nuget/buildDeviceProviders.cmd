b4c723cf37c4816c832d25457565f9a4cac6306b
mort5161 <mnielsen@esri.com>
2016-06-01 16:15:35 -0700

    include winmd referencing .targets file

--- src/Nuget/buildDeviceProviders.cmd
+++ src/Nuget/buildDeviceProviders.cmd
@@ -3,5 +3,6 @@ XCOPY ..\Release\DeviceProviders\DeviceProviders.dll DeviceProviders\runtimes\wi
 XCOPY ..\x64\Release\DeviceProviders\DeviceProviders.dll DeviceProviders\runtimes\win10-x64\native\ /Y
 XCOPY ..\ARM\Release\DeviceProviders\DeviceProviders.dll DeviceProviders\runtimes\win10-arm\native\ /Y
 XCOPY dotMorten.AllJoyn.DeviceProviders.nuspec DeviceProviders\ /Y
+XCOPY dotMorten.AllJoyn.DeviceProviders.targets DeviceProviders\build\native\ /Y
 nuget pack DeviceProviders\dotMorten.AllJoyn.DeviceProviders.nuspec
 RMDIR DeviceProviders /S /Q
