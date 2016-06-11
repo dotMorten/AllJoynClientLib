101e48f7f895eb09eb9dfc2ce0253d0503d002a7
mort5161 <mnielsen@esri.com>
2016-06-11 09:03:20 -0700

    Version 1.1.0

--- src/AllJoynDeviceLib/Properties/AssemblyInfo.cs
+++ src/AllJoynDeviceLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AllJoynClientLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/dotMorten/AllJoynClientLib")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Morten Nielsen")]
 [assembly: AssemblyProduct("AllJoynClientLib")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Morten Nielsen 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: ComVisible(false)]
