d1cd565e161ef272d01fe78e5990ea75a583013d
unknown <mort5161@MININT-1I7MQ6O.esri.com>
2016-08-17 21:28:56 -0700

    Updated grid panel to use UWP Community Toolkit's AdaptiveGrid

---
+++ src/AllJoynSampleApp/Controls/TemplatePickerContentControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace AllJoynSampleApp.Controls
+{
+    public class TemplatePickerContentControl : ContentControl
+    {
+        protected override void OnContentTemplateSelectorChanged(DataTemplateSelector oldContentTemplateSelector, DataTemplateSelector newContentTemplateSelector)
+        {
+            base.OnContentTemplateSelectorChanged(oldContentTemplateSelector, newContentTemplateSelector);
+        }
+
+        public DataTemplateSelector ItemTemplateSelector
+        {
+            get { return (DataTemplateSelector)GetValue(ItemTemplateSelectorProperty); }
+            set { SetValue(ItemTemplateSelectorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemTemplateSelectorProperty =
+            DependencyProperty.Register("ItemTemplateSelector", typeof(DataTemplateSelector), typeof(TemplatePickerContentControl), new PropertyMetadata(null));
+
+
+    }
+}
